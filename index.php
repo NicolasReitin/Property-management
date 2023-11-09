6a0f6956447a43598b86937fc379fbd7945388c4
Nicolas <reitin.pro@gmail.com>
2023-11-09 12:27:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+    require_once('common/conn.php');
+    if ($bdd){
+        echo 'connected';
+    }else{
+        echo 'disconnected';
+    }
+
+
+?>
+    <!-- Gestionnaire de propriétés :
+Développez un gestionnaire de produits où vous pouvez ajouter, modifier et supprimer des produits. 
+Chaque produit peut être une instance d'une classe. -->
+
+<!-- liste de maison -->
+
+
+
+
+<!-- formulaire d'ajout -->
+
+
+
+
+
+    
+</body>
+</html>
