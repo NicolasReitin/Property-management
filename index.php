ab55096702611523300d5c19755513faf07f97a9
Nicolas <reitin.pro@gmail.com>
2023-11-09 15:03:36 +0100

    update

--- index.php
+++ index.php
@@ -82,8 +82,8 @@ Chaque produit peut être une instance d'une classe. -->
                     <td><?= $property['adresse'] ?></td>
                     <td><?= $property['surface'] ?></td>
                     <td><?= $property['prix'] ?></td>
-                    <td><a href=""><button class="btn btn-warning" type="button">Edit</button></a></td>
-                    <td><button class="btn btn-danger" type="button">X</button></td>
+                    <td><a href="controllers/editProperty.php?id=<?=$property['id']?>"><button class="btn btn-warning" type="button">Edit</button></a></td>
+                    <td><a href="controllers/editProperty.php?id=<?=$property['id']?>"><button class="btn btn-danger" type="button">X</button></a></td>
                 </tr>
 
                 <?php
@@ -100,30 +100,6 @@ Chaque produit peut être une instance d'une classe. -->
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<?php 
-    require_once('common/conn.php');
-    if ($bdd){
-        echo 'connected';
-    }else{
-        echo 'disconnected';
-    }
-
-
-?>
-    <!-- Gestionnaire de propriétés :
-Développez un gestionnaire de produits où vous pouvez ajouter, modifier et supprimer des produits. 
-Chaque produit peut être une instance d'une classe. -->
-
-<!-- liste de maison -->
-
-
-
-
-<!-- formulaire d'ajout -->
-
-
-
-
 
 </body>
 </html>
