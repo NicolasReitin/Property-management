26a11ad918f9fe8ad429b0146797e3514f7b3b18
Nicolas <reitin.pro@gmail.com>
2023-11-09 15:05:57 +0100

    update

--- index.php
+++ index.php
@@ -94,36 +94,7 @@ Chaque produit peut être une instance d'une classe. -->
     </tbody>
 </table>
 
-
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<?php 
-    require_once('common/conn.php');
-    if ($bdd){
-        echo 'connected';
-    }else{
-        echo 'disconnected';
-    }
-
-
-?>
-    <!-- Gestionnaire de propriétés :
-Développez un gestionnaire de produits où vous pouvez ajouter, modifier et supprimer des produits. 
-Chaque produit peut être une instance d'une classe. -->
-
-<!-- liste de maison -->
-
-
-
-
-<!-- formulaire d'ajout -->
-
-
-
-
 
 </body>
 </html>
